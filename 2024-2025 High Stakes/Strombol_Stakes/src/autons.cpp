0d0673e4b3ce8e5ea59b95762f1f382eb6d5133e
HyruleCoCo <156859125+HyruleCoCo@users.noreply.github.com>
2024-11-16 21:17:34 -0600

    UIUC comp

--- 2024-2025 High Stakes/Strombol_Stakes/src/autons.cpp
+++ 2024-2025 High Stakes/Strombol_Stakes/src/autons.cpp
@@ -1,7 +1,7 @@
 #include "vex.h"
 
 int grab(){
-  wait(810, msec);
+  wait(880, msec);
   msClamp.set(true);
   return 0;
 }
@@ -10,147 +10,282 @@ void redPos(){
   chassis.set_coordinates(-63, -23.4, 270);
   // grab the first ms
   vex::task mS(grab);
-  chassis.drive_to_point(-33, -23.567);// 270
+  chassis.drive_to_point(-38, -23.4);// 270
   chassis.drive_max_voltage = 4;// go to a lower voltage to make sure we get the ms
-  chassis.drive_distance(-10);
-  chassis.drive_max_voltage = 10;// resets voltage
+  chassis.drive_distance(-11);
+  chassis.drive_max_voltage = 9;// resets voltage
   convey.spin(fwd, 100, pct);
   wait(1, sec);
   // get the secong ring
-  chassis.drive_distance(5);
+  chassis.drive_distance(6);
   chassis.turn_to_angle(180);
-  chassis.drive_to_point(-27.596, -43.744);// 145
+  chassis.drive_to_point(-33, -43.744);// 145
   wait(1.5, sec);
-  chassis.drive_distance(5);
-  msClamp.set(false);
+  chassis.drive_distance(6);
   //chassis.turn_to_angle(270);
   
   /*chassis.drive_to_point(-40, -2.5);// 0  possibly doink a ring stack, probably not going to happen for now
   chassis.drive_to_point(-40, -2.5);// 270
   chassis.drive_to_point(-42.144, -4.297);// 235*/
+  wait(500, msec);
+  chassis.drive_distance(-6);
   chassis.turn_to_angle(0);
   convey.stop();
-  chassis.drive_to_point(-12, 0);// 90 
+  chassis.drive_to_point(-26, 0);// 90 
 }
 
 void redNeg(){
   chassis.set_coordinates(-63, 23.4, 270);
   // grab the first ms
   vex::task mS(grab);
-  chassis.drive_to_point(-33, 23.567);// 270
+  chassis.drive_to_point(-38, 23.4);// 270
   chassis.drive_max_voltage = 4;// go to a lower voltage to make sure we get the ms
-  chassis.drive_distance(-10);
-  chassis.drive_max_voltage = 10;// resets voltage
+  chassis.drive_distance(-11);
+  chassis.drive_max_voltage = 9;// resets voltage
   convey.spin(fwd, 100, pct);
-  wait(500, msec);
-  chassis.drive_distance(4.5);
-  // grab first ring stack
+  wait(1, sec);
+  // get the secong ring
+  chassis.drive_distance(6);
   chassis.turn_to_angle(0);
-  //chassis.drive_to_point(-34.408, 33.374);// 0
-  chassis.drive_to_point(-27.27, 41.245);// 30
-  chassis.drive_max_voltage = 7;
-  chassis.drive_distance(8.25);
-  wait(500, msec);
-
-  // grab line rings
-  /*chassis.drive_distance(2);
-  chassis.turn_to_angle(90);
-  chassis.drive_to_point(-12, 43.27);// 90
-  chassis.drive_to_point(-17.345, 48.333);// 0
-  chassis.turn_to_angle(75);
-  chassis.drive_to_point(-10.459, 50.764);// 90
-  convey.stop();
-  chassis.drive_distance(-10);*/
+  chassis.drive_to_point(-33, 43.744);// 145
+  wait(1.5, sec);
+  chassis.drive_distance(6);
+  //chassis.turn_to_angle(270);
   
+  /*chassis.drive_to_point(-40, -2.5);// 0  possibly doink a ring stack, probably not going to happen for now
+  chassis.drive_to_point(-40, -2.5);// 270
+  chassis.drive_to_point(-42.144, -4.297);// 235*/
+  wait(500, msec);
+  chassis.drive_distance(-6);
   chassis.turn_to_angle(180);
-  wait(5, sec);
   convey.stop();
-  //chassis.drive_to_point(-32, 3);// 90 
-/*
-  // go and clear corner
-  chassis.drive_to_point(-57.245, 63.929);// 0
-  chassis.turn_to_angle(0);
-  doink.set(true);
-  chassis.turn_to_angle(270);
-  //chassis.drive_to_point(-57.853, 60);// 270
-  chassis.drive_to_point(-30.51, 5.598);// 135 */
+  chassis.drive_to_point(-26, 0);// 90  
 }
 
 void bluePos(){
   chassis.set_coordinates(63, -23.4, 90);
   // grab the first ms
   vex::task mS(grab);
-  chassis.drive_to_point(33, -23.567);// 270
+  chassis.drive_to_point(38, -23.4);// 270
   chassis.drive_max_voltage = 4;// go to a lower voltage to make sure we get the ms
-  chassis.drive_distance(-10);
-  chassis.drive_max_voltage = 10;// resets voltage
+  chassis.drive_distance(-11);
+  chassis.drive_max_voltage = 9;// resets voltage
   convey.spin(fwd, 100, pct);
   wait(1, sec);
   // get the secong ring
-  chassis.drive_distance(5);
+  chassis.drive_distance(6);
   chassis.turn_to_angle(180);
-  chassis.drive_to_point(27.596, -43.744);// 145
+  chassis.drive_to_point(33, -43.744);// 145
   wait(1.5, sec);
-  chassis.drive_distance(5);
-  msClamp.set(false);
-  //chassis.turn_to_angle(270);
+  chassis.drive_distance(6);
+  //chassis.turn_to_angle(270);x
   
   /*chassis.drive_to_point(-40, -2.5);// 0  possibly doink a ring stack, probably not going to happen for now
   chassis.drive_to_point(-40, -2.5);// 270
   chassis.drive_to_point(-42.144, -4.297);// 235*/
+  wait(500, msec);
+  chassis.drive_distance(-6);
   chassis.turn_to_angle(0);
   convey.stop();
-  chassis.drive_to_point(12, 0);// 90
+  chassis.drive_to_point(26, 0);// 90 
 }
 
 void blueNeg(){
   chassis.set_coordinates(63, 23.4, 90);
   // grab the first ms
   vex::task mS(grab);
-  chassis.drive_to_point(33, 23.567);// 270
+  chassis.drive_to_point(38, 23.4);// 270
+  chassis.drive_max_voltage = 4;// go to a lower voltage to make sure we get the ms
+  chassis.drive_distance(-11);
+  chassis.drive_max_voltage = 9;// resets voltage
+  convey.spin(fwd, 100, pct);
+  wait(1, sec);
+  // get the secong ring
+  chassis.drive_distance(6);
+  chassis.turn_to_angle(0);
+  chassis.drive_to_point(33, 43.744);// 145
+  wait(1.5, sec);
+  chassis.drive_distance(6);
+  //chassis.turn_to_angle(270);
+  
+  /*chassis.drive_to_point(-40, -2.5);// 0  possibly doink a ring stack, probably not going to happen for now
+  chassis.drive_to_point(-40, -2.5);// 270
+  chassis.drive_to_point(-42.144, -4.297);// 235*/
+  wait(500, msec);
+  chassis.drive_distance(-6);
+  chassis.turn_to_angle(180);
+  convey.stop();
+  chassis.drive_to_point(26, 0);// 90 
+}
+
+
+int snatch2(){
+  wait(405, msec);
+  msClamp.set(true);
+  return 0;
+}
+void skills(){
+  chassis.set_coordinates(-75, 23, 270);
+
+  // grab first ms
+  vex::task msSnatch(snatch2);   
+  chassis.drive_to_point(-56.004, 23.697);// 270
   chassis.drive_max_voltage = 4;// go to a lower voltage to make sure we get the ms
   chassis.drive_distance(-10);
   chassis.drive_max_voltage = 10;// resets voltage
   convey.spin(fwd, 100, pct);
-  wait(500, msec);
-  chassis.drive_distance(4.5);
-  // grab first ring stack
-  chassis.turn_to_angle(0);
-  //chassis.drive_to_point(-34.408, 33.374);// 0
-  chassis.drive_to_point(27.27, 41.245);// 30
-  chassis.drive_max_voltage = 7;
-  chassis.drive_distance(8.25);
-  wait(500, msec);
+  wait(1, sec);
 
-  // grab line rings
-  /*chassis.drive_distance(2);
+  // grab first ring
   chassis.turn_to_angle(90);
-  chassis.drive_to_point(-12, 43.27);// 90
-  chassis.drive_to_point(-17.345, 48.333);// 0
-  chassis.turn_to_angle(75);
-  chassis.drive_to_point(-10.459, 50.764);// 90
-  convey.stop();
-  chassis.drive_distance(-10);*/
-  
-  chassis.turn_to_angle(180);
-  wait(5, sec);
-  convey.stop();
-  //chassis.drive_to_point(-32, 3);// 90 
-/*
-  // go and clear corner
-  chassis.drive_to_point(-57.245, 63.929);// 0
-  chassis.turn_to_angle(0);
-  doink.set(true);
+  chassis.drive_to_point(-23.057, 23.057);// 90
+  wait(500, msec);
+
+  // grab second ring
+  chassis.drive_to_point(-24.016, 49.607);// 0
+  wait(500, msec);
+
+  // grab thrid & fourth ring
   chassis.turn_to_angle(270);
-  //chassis.drive_to_point(-57.853, 60);// 270
-  chassis.drive_to_point(-30.51, 5.598);// 135 */
+  chassis.drive_max_voltage = 7;
+  chassis.drive_to_point(-63.361, 46.088);// 45
+  wait(500, msec);
+
+  // grab fifth ring
+  chassis.drive_to_point(-45.448, 60.482);// 100
+
+  // put ms in corner
+  chassis.turn_to_angle(110);
+  chassis.drive_to_point(-60, 59);// 135
+  msClamp.set(false);
+
+  // grab another ring
+  chassis.drive_to_point(-3.864, 2.265);// 0
+  wait(250, msec);
+  convey.stop(hold);
+
+  // grab next ms
+  chassis.drive_to_point(-23.377, -9.251);// 0
+  chassis.turn_to_angle(45);
+  vex::task mS(grab);
+  chassis.drive_to_point(-41.29, -18.847);// 45
+  chassis.drive_max_voltage = 4;// go to a lower voltage to make sure we get the ms
+  chassis.drive_distance(-10);
+  chassis.drive_max_voltage = 10;// resets voltage
+  convey.spin(fwd, 100, pct);
+  wait(1, sec);
+
+  // grab first ring
+  chassis.turn_to_angle(135);
+  chassis.drive_to_point(-23.057, -24.925);// 0
+  wait(500, msec);
+
+  // grab second ring
+  chassis.turn_to_angle(180);
+  chassis.drive_to_point(-24.656, -46.037);// 0
+  wait(500, msec);
+
+  // grab third & fourth ring
+  chassis.drive_max_voltage = 7;
+  chassis.drive_to_point(-66.24, -45.077);// 0
+  wait(500, msec);
+  chassis.drive_max_voltage = 10;
+
+  // grab fifth ring
+  chassis.drive_to_point(-46.728, -46.037);// 0
+  chassis.turn_to_angle(180);
+  chassis.drive_to_point(-47.687, -56.593);// 0
+  wait(500, msec);
+
+  // put ms in corner
+  chassis.turn_to_angle(45);
+  chassis.drive_to_point(-61.442, -57.552);// 45
+  msClamp.set(false);
+  wait(200, msec);
+
+  // grab next ms
+  chassis.drive_to_point(23.325, -33.562);// 0
+  convey.stop();
+  chassis.turn_to_angle(225);
+  vex::task ms(grab);
+  chassis.drive_to_point(48.596, -24.285);// 260
+  chassis.drive_max_voltage = 4;// go to a lower voltage to make sure we get the ms
+  chassis.drive_distance(-10);
+  chassis.drive_max_voltage = 10;// resets voltage
+
+  // put in corner
+  chassis.turn_to_angle(180);
+  chassis.drive_to_point(62.351, -58.512);// 325
+  doink.set(true);
+  chassis.turn_to_angle(0);
+  msClamp.set(false);
+  doink.set(false);
+
+  // grab next ms
+  chassis.drive_to_point(59.472, 0.665);// 0
+  chassis.turn_to_angle(180);
+  vex::task msGrab(snatch2);
+  chassis.drive_to_point(59.152, 14.74);// 180
+  chassis.drive_max_voltage = 4;// go to a lower voltage to make sure we get the ms
+  chassis.drive_distance(-10);
+  chassis.drive_max_voltage = 10;// resets voltage
+
+  // put ms in corner
+  chassis.turn_to_angle(0);
+  chassis.drive_to_point(63.63, 59.523);// 225
+  doink.set(true);
+  chassis.turn_to_angle(135);
+  wait(40, msec);
+  chassis.turn_to_angle(180);
+  msClamp.set(false);
+  doink.set(false);
+
+  // grab next ms
+  chassis.drive_to_point(46.996, 25.296);// 0
+  chassis.turn_to_angle(0);
+  vex::task msGrabs(snatch2);
+  chassis.drive_to_point(46.357, 6.423);// 0
+  chassis.drive_max_voltage = 4;// go to a lower voltage to make sure we get the ms
+  chassis.drive_distance(-10);
+  chassis.drive_max_voltage = 10;// resets voltage
+
+  // grab first ms
+  convey.spin(fwd, 100, pct);
+  chassis.drive_to_point(22.046, 23.377);// 0
+  wait(500, msec);
+
+  // grab second ms
+  chassis.drive_to_point(21.406, 49.607);// 0
+  wait(500, msec);
+
+  // grab thrid ms
+  chassis.drive_to_point(-0.345, 58.563);// 0
+  wait(300, msec);
+
+  // grab fourth ms
+  chassis.drive_to_point(34.521, 29.454);// 0
+  chassis.turn_to_angle(180);
+  chassis.drive_to_pose(36, -3, 200);
+  chassis.drive_to_point(22.686, -23.325);// 0
+  wait(300, msec);
+
+  // grab fifth ms
+  chassis.drive_to_pose(24.285, -47.316, 200);// 0
+
+  // grab sixth ms
+  chassis.drive_to_point(2.533, -59.152);// 0
+  msClamp.set(false);
+
+  // donuts
+  chassis.drive_to_point(-8, -40);
+  chassis.DriveL.spin(fwd, 100, pct);
 }
 
-void skills(){
-
+void donuts(){
+  chassis.drive_distance(-10);
+  chassis.DriveL.spin(fwd, 100, pct);
+  chassis.DriveR.spin(reverse, 100, pct);
 }
-
-
 /**
  * Resets the constants for auton movement.
  * Modify these to change the default behavior of functions like
@@ -176,7 +311,7 @@ void default_constants(){
 
   chassis.turn_kp =1;
   chassis.turn_ki = 0.09;
-  chassis.turn_kd = 7.05;
+  chassis.turn_kd = 10;
 
   // Each exit condition set is in the form of (settle_error, settle_time, timeout).
   chassis.set_drive_exit_conditions(1.5, 300, 5000);
